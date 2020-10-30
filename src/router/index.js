07cfcf7c331b7d5f08dfa81d4992259030fa624e
James Qiu <qiuqiang_keepfaith@163.com>
2020-10-31 01:38:53 +0800

    广告推荐和广告推荐位的两个页面的index列表的增删改查和form表单，但很多前端是copy的不能坐视不管

--- src/router/index.js
+++ src/router/index.js
@@ -118,6 +118,58 @@ export const constantRouterMap = [
         hidden: true
       }
     ]
+  },
+
+  // 广告推荐与推荐位路由配置
+  {
+    path: '/ad',
+    component: Layout,
+    redirect: '/ad/list',
+    name: 'Ad',
+    meta: { title: '内容管理' },
+    children: [
+      {
+        path: 'list',
+        name: 'AdList',
+        component: () => import('@/views/ad/list'),
+        meta: { title: '广告推荐' }
+      },
+      {
+        path: 'create',
+        name: 'AdCreate',
+        component: () => import('@/views/ad/form'),
+        meta: { title: '添加广告推荐' },
+        hidden: true
+      },
+      {
+        path: 'edit/:id',
+        name: 'AdEdit',
+        component: () => import('@/views/ad/form'),
+        meta: { title: '修改广告推荐' },
+        hidden: true
+      },
+
+      {
+        path: 'type-list',
+        name: 'AdTypeList',
+        component: () => import('@/views/adType/list'),
+        meta: { title: '推荐位' }
+      },
+      {
+        path: 'type-create',
+        name: 'AdTypeCreate',
+        component: () => import('@/views/adType/form'),
+        meta: { title: '添加推荐位' },
+        hidden: true
+      },
+      {
+        path: 'type-edit/:id',
+        name: 'AdTypeEdit',
+        component: () => import('@/views/adType/form'),
+        meta: { title: '修改推荐位' },
+        hidden: true
+      }
+    ]
   }
 ]
 
